147e33f5066031a32df2a12ff926d8126cf1e8b8
Sadturtleman <rugsn12345@gmail.com>
2026-02-19 13:25:39 +0900

    feat: versel config add

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Front-end/',
   server: {
     proxy: {
       '/api': {
